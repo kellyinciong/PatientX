3ec29b5d76f56ff16bf29f9b5b93f6dce66e9e24
Kelly Inciong <kinciong@uci.edu>
2014-11-15 09:28:06 -0800

    Login screen added, but didn't connect it yet

---
+++ PatientX/PatientX/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  PatientX
+//
+//  Created by Kelly Inciong on 11/15/14.
+//  Copyright (c) 2014 Team4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
